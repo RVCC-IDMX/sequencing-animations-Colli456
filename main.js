5a195d7a8ffd8194e24ee989d3ce4825373c3095
Colli456 <cjclarke14y@gmail.com>
2024-04-13 23:17:40 -0400

    Callback Animation

--- main.js
+++ main.js
@@ -9,9 +9,24 @@ const aliceTiming = {
   fill: 'forwards'
 }
 
+// callback
 const alice1 = document.querySelector("#alice1");
+// const animation1 = alice1.animate(aliceTumbling, aliceTiming);
+// animation1.onfinish = function() {
+//   const animation2 = alice2.animate(aliceTumbling, aliceTiming);
+//   animation2.onfinish = function() {
+//     const animation3 = alice3.animate(aliceTumbling, aliceTiming);
+//   }
+// }
+
+
 const alice2 = document.querySelector("#alice2");
+
+
+
 const alice3 = document.querySelector("#alice3");
+
+
 
 // const animation1 = alice1.animate(aliceTumbling, aliceTiming);
 // console.log(animation1);
